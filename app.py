bb83d7080ced3368e19dd5be357e943075795186
thisispaniz <paniz.ziaie@gmail.com>
2024-02-01 11:24:19 +0100

    The functionality is done

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+booklist = Flask(__name__)
+booklist.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(booklist)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(50), nullable=False)
+    author = db.Column(db.String(50), nullable=False)
+    publication_year = db.Column(db.Integer, nullable=True) 
+    
+with booklist.app_context():
+    db.create_all()
+    
+@booklist.route('/books')
+def books():
+    books = Book.query.all()
+    return render_template('books.html', books=books)
+
+@booklist.route('/add_book', methods=['GET', 'POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        publication_year = request.form['publication_year']
+        added_book = Book(title=title, author=author, publication_year=publication_year)
+        db.session.add(added_book)
+        db.session.commit()
+        return redirect('/')
+    return render_template('add_book.html')
+
+if __name__ == "__main__":
+    booklist.run(debug=True)
